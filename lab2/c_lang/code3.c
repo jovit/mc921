8a1c91b962b827470ad3784c7e691fb86cf403d5
João Vitor Araki Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-08-21 21:40:06 -0300

    .

--- lab2/c_lang/code3.c
+++
@@ -1,4 +0,0 @@
-INT 	LONG +,-*>ENUM VOID UNION&:
-0x123Ff||44465.1234&&9632 '5'_aboko
- 	 INT990 )) _aA8Aa_ { ][	;
-#
--- lab2/c_lang/results/code3_corrected.c
+++
@@ -1,4 +0,0 @@
-int 	long +,-*>enum void union&:
-0x123Ff||44465.1234&&9632 '5'_aboko
- 	 INT990 )) _aA8Aa_ { ][	;
-#
